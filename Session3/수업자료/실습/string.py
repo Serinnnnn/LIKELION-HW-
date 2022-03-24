aa2299e683bf812f3d78bd0fc8d1dd82a61fd326
Serinnnnn <ghdtpfls2006@naver.com>
2022-03-24 15:56:32 +0900

    어려워용

---
+++ Session3/수업자료/실습/string.py
@@ -0,0 +1,12 @@
+answer = "3/21은 NEXT 세션하는 날!"
+
+
+string = string1.replace()
+if answer == string:
+    print("정답입니다!")
+else:
+    print("틀렸습니다.")
+    print(f"정답 : {answer}")
+    print(f"입력값 : {string}")
+    print(string1 + string2 + string3)
+
