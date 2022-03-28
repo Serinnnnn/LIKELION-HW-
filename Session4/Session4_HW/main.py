4a6e5969bd7bf19227624f3b65483e3c2df5b877
Serinnnnn <ghdtpfls2006@naver.com>
2022-03-28 19:10:28 +0900

    찐막

--- Session4/Session4_HW/main.py
+++ Session4/Session4_HW/main.py
@@ -1,31 +1,66 @@
+from webtoon import extract_info
 import requests
 from bs4 import BeautifulSoup
-
-from webtoon import extract_info
-
-
+import csv
 
 file = open('webtoon.csv', mode='w', newline='')
+
 writer = csv.writer(file)
-writer.writerow(['title','author','rating'])
+writer.writerow(["title","author","rating"])
+
+final_result =[]
 
 
-Webtoon_URL = f'https://comic.naver.com/webtoon/weekdayList?week=thu'
+Webtoon_URL = f"https://comic.naver.com/webtoon/weekdayList?week=sun"
 webtoon_html = requests.get(Webtoon_URL)
 webtoon_soup = BeautifulSoup(webtoon_html.text,"html.parser")
 
-webtoon_img_list = webtoon_soup.find("ul",{"class":"img_list"})
-webtoon_list = webtoon_img_list.find_all("li")
+weboon_list_box = webtoon_soup.find("ul",{"class":"img_list"})
+webtoon_list = weboon_list_box.find_all("li") 
 
 
-final_result = []
 final_result += extract_info(webtoon_list)
 
-for i in final_result:
-    row = []
-    row.append(i['title'])
-    row.append(i['author'])
-    row.append(i['rating'])
-    writer.writerow(row)
+for result in final_result:
+    row=[]
+    row.append(result['title'])
+    row.append(result['author'])
+    row.append(result['rating'])
+    writer.writerow(row) 
 
-print(final_result)
+
+# 
+# 
+# 
+# 
+# import requests
+# from bs4 import BeautifulSoup
+
+# from webtoon import extract_info
+
+
+
+# file = open('webtoon.csv', mode='w', newline='')
+# writer = csv.writer(file)
+# writer.writerow(['title','author','rating'])
+
+
+# Webtoon_URL = f'https://comic.naver.com/webtoon/weekdayList?week=thu'
+# webtoon_html = requests.get(Webtoon_URL)
+# webtoon_soup = BeautifulSoup(webtoon_html.text,"html.parser")
+
+# webtoon_img_list = webtoon_soup.find("ul",{"class":"img_list"})
+# webtoon_list = webtoon_img_list.find_all("li")
+
+
+# final_result = []
+# final_result += extract_info(webtoon_list)
+
+# for i in final_result:
+#     row = []
+#     row.append(i['title'])
+#     row.append(i['author'])
+#     row.append(i['rating'])
+#     writer.writerow(row)
+
+# print(final_result)
--- Session4/Session4_HW/webtoon.py
+++ Session4/Session4_HW/webtoon.py
@@ -1,34 +1,51 @@
-import requests
-from bs4 import BeautifulSoup
-
-WEBTOON_URL = f'https://comic.naver.com/webtoon/weekdayList?week=sun'
-webtoon_html = requests.get(WEBTOON_URL)
-webtoon_soup = BeautifulSoup(webtoon_html.text,"html.parser")
-print(webtoon_soup)
-
-webtoon_list_box = webtoon_soup.find("ul",{"class":"img_list"})
-webtoon_list = webtoon_list_box.find_all("li")
-
 def extract_info(webtoon_list):
-    result = []
+    result=[]
     
     for webtoon in webtoon_list:
         title = webtoon.find("dl").find("a").string
         author = webtoon.find("dd",{"class":"desc"}).find("a").string
         rating = webtoon.find("div",{"class":"rating_type"}).find("strong").string
-        webtoon_info = {
+
+        webtoon_detail = {
             'title' : title,
             'author' : author,
-            'rating' : rating
+            'rating' : rating,
         }
-        result.append(webtoon_info)
-    print(result)
+        result.append(webtoon_detail)
+    return result
+
+# import requests
+# from bs4 i
+# import BeautifulSoup
+
+# WEBTOON_URL = f'https://comic.naver.com/webtoon/weekdayList?week=sun'
+# webtoon_html = requests.get(WEBTOON_URL)
+# webtoon_soup = BeautifulSoup(webtoon_html.text,"html.parser")
+# print(webtoon_soup)
+
+# webtoon_list_box = webtoon_soup.find("ul",{"class":"img_list"})
+# webtoon_list = webtoon_list_box.find_all("li")
+
+# def extract_info(webtoon_list):
+#     result = []
+    
+#     for webtoon in webtoon_list:
+#         title = webtoon.find("dl").find("a").string
+#         author = webtoon.find("dd",{"class":"desc"}).find("a").string
+#         rating = webtoon.find("div",{"class":"rating_type"}).find("strong").string
+#         webtoon_info = {
+#             'title' : title,
+#             'author' : author,
+#             'rating' : rating
+#         }
+#         result.append(webtoon_info)
+#     print(result)
 
  
 
 
-# title = webtoon_list.find("dl").find("a").string
+# # title = webtoon_list.find("dl").find("a").string
 
 
-# print(title)
-# # dl ->dt->a 
+# # print(title)
+# # # dl ->dt->a 
