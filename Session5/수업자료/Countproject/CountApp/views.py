747f5b94455f8a96ce6315555c61a27d528d028b
Serinnnnn <ghdtpfls2006@naver.com>
2022-03-31 00:34:39 +0900

    세션5hw

---
+++ Session5/수업자료/Countproject/CountApp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def count(request):
+    return render(request, 'count.html')
+
+def result(request):
+    text= request.POST['text']
+    total_len = len(text)
+    no_blank_len = len(text.replace(' ', ''))
+    word_num= len(text.split())
+
+    return render(request, 'result.html', {
+        'total_len': total_len,
+        'no_blank_len': no_blank_len, 
+        'text': text,
+        'word_num':word_num})
+
+    
+
